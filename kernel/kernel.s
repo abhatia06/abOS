1027b45bf2971994b86029966604518ef71548fd
coops-web <angadbhatia122@gmail.com>
2025-06-10 22:27:42 -0500

    Update kernel.s (set up stack? Hopefully? Maybe)

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -1,5 +1,18 @@
-; I need to figure out how to code in 32 bit x86 lol 
-; I am still very VERY new to the x86 ISA. I only took a very basic intro to cs architecture course in university
-; which taught me about the ISA for the LC-3, which was suuppeer easy to understand. 
-; the x86 ISA is a completely different beast lol. The Bootloader alone took me 3-4 days to understand and
-; it's not even done :((
+; this is my first attempt of making a kernel. I realized from OSDev wiki that I needed to re-initialize the stack, because
+; switching to protected mode may have messed up the segment pointers (SS, SP). So, I decided to have the kernel set up the
+; stack. I don't know if this is actually correct, because I have yet to actually research kernels written in assembly.
+; most tutorials seem to immediately set up the linker and kernel.c. I don't want to do that just yet.
+BITS 32
+ORG 0x100000
+
+SECTION .text
+
+set_up_stack:
+
+    MOV ESP, stack_end
+
+SECTION .bss
+
+stack_begin:
+    RESB 8192
+stack_end:
