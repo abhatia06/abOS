7bc3a56c6a814b25a0cec2603ece87c2975a86ee
coops-web <angadbhatia122@gmail.com>
2025-06-11 00:30:13 -0500

    Update kernel.s (correct origin)

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -1,15 +1,28 @@
-; this is my first attempt of making a kernel. I realized from OSDev wiki that I needed to re-initialize the stack, because
-; switching to protected mode may have messed up the segment pointers (SS, SP). So, I decided to have the kernel set up the
-; stack. I don't know if this is actually correct, because I have yet to actually research kernels written in assembly.
-; most tutorials seem to immediately set up the linker and kernel.c. I don't want to do that just yet.
 BITS 32
-ORG 0x100000
+ORG 0x10000
 
 SECTION .text
 
-set_up_stack:
+start:
 
     MOV ESP, stack_end
+
+    MOV EDI, 0xB8000
+    MOV AH, 0x0F
+    MOV ESI, message
+
+.print_loop:
+    LODSB
+    CMP AL, 0
+    JE .done
+    STOSW
+    JMP .print_loop
+
+.done:
+    HLT
+    JMP $
+
+message: db 'Hello World from Kernel!', 0
 
 SECTION .bss
 
