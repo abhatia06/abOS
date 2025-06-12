0caa22471ee78c92e08f6bd21b040b5dc7bf0ffd
coops-web <angadbhatia122@gmail.com>
2025-06-11 21:17:38 -0500

    Update kernel.s

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -1,12 +1,19 @@
 BITS 32
-ORG 0x10000
+
+GLOBAL _start
+EXTERN main
 
 SECTION .text
 
-start:
+_start:
 
     MOV ESP, stack_end
 
+    CALL main
+hang:
+    JMP hang
+
+    # everything below this doesnt get used, but I dont wanna delete it all just yet lol 
     MOV EDI, 0xB8000
     MOV AH, 0x0F
     MOV ESI, message
