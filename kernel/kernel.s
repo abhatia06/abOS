c177fb9c6a497513f0594a1ecc2dcc19f1453cad
coops-web <angadbhatia122@gmail.com>
2025-06-11 21:20:06 -0500

    Update kernel.s (better comments)

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -7,13 +7,14 @@ SECTION .text
 
 _start:
 
-    MOV ESP, stack_end
-
+    MOV ESP, stack_end     ; This sets up the stack.
+    ; I ended up learning about external calls and whatnot through THIS video below:
+    ; https://www.youtube.com/watch?v=jDZuTpCE8l8
     CALL main
 hang:
     JMP hang
 
-    # everything below this doesnt get used, but I dont wanna delete it all just yet lol 
+    ; everything below this doesnt get used, but I dont wanna delete it all just yet lol 
     MOV EDI, 0xB8000
     MOV AH, 0x0F
     MOV ESI, message
