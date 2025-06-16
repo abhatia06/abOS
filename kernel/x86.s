39a66174bf49a16f771df7d7ca4018b998ef04da
coops-web <angadbhatia122@gmail.com>
2025-06-16 15:19:39 -0500

    Update x86.s (used EBP instead of ESP)

--- kernel/x86.s
+++ kernel/x86.s
@@ -9,20 +9,20 @@ SECTION _text:
 GLOBAL _x86_Video_WriteCharTeletype
 _x86_Video_WriteCharTeletype:
 
-     PUSHA
-     MOV EBX, VIDEO_MEMORY      ; We want to display to here
-     ADD EBX,  [ESP + 44]        ; look at comment below to get explanation as to why we add 44
+     PUSH EBP                    ; Nanobyte uses BP, and a lot of other people use BP too, so  I decided to use it as well. Here we save the old frame data
+     MOV EBP, ESP                ; And we load in the new frame data 
 
-; ESP + 4 is the character we want to display. ESP is the stack pointer, it points to the return address since this function is
-; being called. But, since we're using CDECL, (GCC automatically uses it), and stacks are LIFO, if we add 4 to the memory address
-; that ESP points to, we get the character we want to display. (Each 'thing' in the stack takes up 4 bytes, that's why we add 4).
-; CORRECTION: You may have seen the explanation above and asked why we're adding 44 and 36 instead. That's because the first line
-; we call is PUSHA, which pushes a bunch of general purpose registers into the stack, so we have to add even MORE to the ESP to 
-; make it work.
+; ESP + 8 is the character we want to display. ESP is the stack pointer, it points to the return address since this function is
+; being called. But, we also pushed EBP into the stack, so ESP actually now points to EBP. Also, since we're using CDECL, and each argument
+; was passed from right to left, that means the argument that is furthest away from ESP is the position variable, followed by the page variable, followed by the character
+; finally followed by the return address. Since each element in the stack takes up 4 bytes, we have to add 4 at a time to skip certain things in the stack. Hence, we do
+; ESP + 8 for the character.
 
-     MOVZX EAX, BYTE [ESP + 36]  ; EAX is just the 32-bit version of the 16-bit register AX. Literally. AX represents the lower
-                                 ; 16 bits of EAX.
+     MOVZX EAX, BYTE [EBP + 8] 
+     MOV EBX, VIDEO_MEMORY
+     ADD EBX, [EBP + 16]     ; offset 
      MOV AH, WHITE_ON_BLACK
-     MOV [EBX], AX
-     POPA          ; IMPORTANT, because ESP is pointing to some random GPR, not the return address.
+     MOV EBX, AX
+     
+     POP EBP          ; Return EBP back to its original address
      RET
