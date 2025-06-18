9c9c0b28ca3207af2f1b27bc208e728118e1a6d4
crimlegfish <angadbhatia122@gmail.com>
2025-06-18 17:33:42 -0500

    Update x86.h

--- kernel/x86.h
+++ kernel/x86.h
@@ -1,9 +1,6 @@
 #include "stdint.h"
-#ifndef x86_H
-#define x86_H
+#pragma once
 
 void _x86_div64_32(uint64_t dividend, uint32_t divisor, uint64_t* quotient, uint32_t* remainderOut);
 
 void _x86_Video_WriteCharTeletype(char c, uint8_t page, uint8_t pos);
-
-#endif
