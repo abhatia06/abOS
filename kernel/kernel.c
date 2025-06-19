bfdbf55315c988dda90f0b2a41a4c467c2ea0a25
crimlegfish <angadbhatia122@gmail.com>
2025-06-19 12:02:21 -0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,9 +3,9 @@
 
 void main() {
 	const char* not_really_far_string = "far string";
-	puts("Hello World from C!");
-	kprintf("Formatted %% %c %s %ls" 'a', "string", not_really_far_string);
-	kprintf("Formatted %d %i %x %p %o %hd %hi %hhu %hhd", 1234, -5678, 0xdead, 0xbeef, 012345, (short)27, (short)-42, (unsigned char)20, (signed char)-10);
-	kprintf("Formatted %ld %lx %lld %llx", -100000000l, 0xdeadbeeful, 10200300400ll, 0xdeadbeeffeebdaedull);
+	puts("Hello World from C!\n");
+	kprintf("Formatted %% %c %s %ls\n" 'a', "string", not_really_far_string);
+	kprintf("Formatted %d %i %x %p %o %hd %hi %hhu %hhd\n", 1234, -5678, 0xdead, 0xbeef, 012345, (short)27, (short)-42, (unsigned char)20, (signed char)-10);
+	kprintf("Formatted %ld %lx %lld %llx\n", -100000000l, 0xdeadbeeful, 10200300400ll, 0xdeadbeeffeebdaedull);
 	return;
 }
