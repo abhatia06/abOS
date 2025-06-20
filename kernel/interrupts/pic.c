b523695019ed87f028093eacd12478d4d2108668
crimlegfish <angadbhatia122@gmail.com>
2025-06-19 21:17:04 -0500

    Update pic.c (IRQ and whatnot)

--- kernel/interrupts/pic.c
+++ kernel/interrupts/pic.c
@@ -28,8 +28,57 @@ void PIC_remap(int offset1, int offset2) {
 	outb(PIC2_DATA, 0);	
 }
 
-//  Disables interrupts 
+//  Disables interrupts. Really only had this for testing, don't think I'll ever use it? After today, I've decided to once again have tomorrow
+//  be a study day, because this is all a tad bit confusing, and I would like to understand it before moving onto the next step, which is 
+//  actually implementing various ISR's. 
 void pic_disable() {
 	outb(PIC1_DATA, 0xFF);
 	outb(PIC2_DATA, 0xFF);
+}
+
+void IRQ_set_mask(uint8_t IRQline) {
+        uint16_t port;
+        uint8_t value;
+
+        if(IRQline < 8) {
+                port = PIC1_DATA;
+        }
+        else {
+                port = PIC2_DATA;
+                IRQline -= 8;
+        }
+        value = inb(port) | (1 << IRQline);
+        outb(port, value);
+}
+
+void IRQ_clear_mask(uint8_t IRQline) {
+        uint16_t port;
+        uint8_t value;
+
+        if(IRQline < 8) {
+                port = PIC1_DATA;
+        }
+        else {
+                port = PIC2_DATA;
+                IRQline -= 8;
+        }
+        value = inb(port) & ~(1 << IRQline);
+        outb(port, value);
+}
+
+#define PIC_READ_IRR    0x0A
+#define PIC_READ_ISR    0x0B
+
+static uint16_t __pic_get_irq_reg(int ocw3) {
+        outb(PIC1_COMMAND, ocw3);
+        outb(PIC2_COMMAND, ocw3);
+        return (inb(PIC2_COMMAND) << 8) | inb(PIC1_COMMAND);
+}
+
+uint16_t pic_get_irr(void) {
+        return __pic_get_irq_reg(PIC_READ_IRR);
+}
+
+uint16_t pic_get_isr(void) {
+        return __pic_get_irq_reg(PIC_READ_ISR);
 }
