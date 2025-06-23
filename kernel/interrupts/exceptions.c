d3d653b81f6047963a96a3a661048fbf36c1b642
crimlegfish <angadbhatia122@gmail.com>
2025-06-23 18:06:18 -0500

    Update exceptions.c (keyboard handling!!)

--- kernel/interrupts/exceptions.c
+++ kernel/interrupts/exceptions.c
@@ -3,9 +3,23 @@
 #include "idt.h"
 #include <stdbool.h>
 #include "exceptions.h"
+#include "pic.h"
+#include "../x86.h"
 
 __attribute__((interrupt)) void div_by_0_handler(int_frame_32_t *frame) {
         kprintf("DIVIDE BY 0 ERROR\r\n");
 
         __asm__ volatile ("cli ; hlt");
+}
+
+__attribute__((interrupt)) void keyboard_handler(int_frame_32_t *frame) {
+        uint8_t keyboarddata = inb(0x60);
+
+        if(scancode & 0x80) {
+                // nothing for now
+        }
+        else {
+                kprintf("key pressed\r\n");
+        }
+        PIC_sendEOI(1);
 }
