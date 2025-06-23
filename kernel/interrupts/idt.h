213001b8a2188c4e1dc1ad7bd4c1a4814f41518a
crimlegfish <angadbhatia122@gmail.com>
2025-06-23 13:27:22 -0500

    Update idt.h (fixed headers)

--- kernel/interrupts/idt.h
+++ kernel/interrupts/idt.h
@@ -17,4 +17,11 @@ typedef struct {
 void idt_set_descriptor(uint8_t vector, void* isr, uint8_t flags);
 void initIDT(void);
 
-__attribute__((noreturn)) void exception_handler(void);
+__attribute__((noreturn)) void exception_handler(uint32_t vector_number);
+__attribute__((noreturn)) void exception_handler_error(uint32_t vector_number, uint32_t error_code);
+
+typdef struct {
+	uint32_t eflags;
+	uint32_t cs;
+	uint32_t eip;
+} __attribute__((packed)) int_frame_32_t;
