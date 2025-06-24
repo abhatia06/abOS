53083d249053f2a167250192435bf1bf3d55db58
crimlegfish <angadbhatia122@gmail.com>
2025-06-24 17:25:30 -0500

    Update kernel.c (inputs!)

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -22,5 +22,9 @@ void main() {
 	kprintf("Formatted %% %c %s %ls\n" 'a', "string", not_really_far_string);
 	kprintf("Formatted %d %i %x %p %o %hd %hi %hhu %hhd\n", 1234, -5678, 0xdead, 0xbeef, 012345, (short)27, (short)-42, (unsigned char)20, (signed char)-10);
 	kprintf("Formatted %ld %lx %lld %llx\n", -100000000l, 0xdeadbeeful, 10200300400ll, 0xdeadbeeffeebdaedull);
+
+	kprintf("Type your name: ");
+	char* name = readline();
+	kprintf("Hello, %s!\n", name);
 	return;
 }
