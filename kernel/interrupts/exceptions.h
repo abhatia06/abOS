dcd773028de6ae56ea87486e550108b37e2554f6
crimlegfish <angadbhatia122@gmail.com>
2025-06-24 17:26:35 -0500

    Update exceptions.h (input buffers and whatnot)

--- kernel/interrupts/exceptions.h
+++ kernel/interrupts/exceptions.h
@@ -1,4 +1,8 @@
 #pragma once
 
+extern char input_buffer[256];
+extern unsigned int input_pos;
+extern bool input_ready;
+
 void div_by_0_handler(int_frame_32_t *frame);
 void keyboard_handler(int_frame_32_t *frame);
