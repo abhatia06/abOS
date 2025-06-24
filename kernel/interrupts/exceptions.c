31c9c44ad3b5aa0f8d6ac35de8db87c38825b03a
crimlegfish <angadbhatia122@gmail.com>
2025-06-24 13:06:00 -0500

    Update exceptions.c (actual text display)

--- kernel/interrupts/exceptions.c
+++ kernel/interrupts/exceptions.c
@@ -5,15 +5,7 @@
 #include "exceptions.h"
 #include "pic.h"
 #include "../x86.h"
-
-// These codes are the hex ASCII code for the keys they represent.
-#define LSHIFT_MAKE     0x2A        
-#define LSHIFT_BREAK    0xAA
-#define RSHIFT_MAKE     0x36
-#define RSHIFT_BREAK    0xB6
-#define LCTRL_MAKE      0x1D
-#define LCTRL_BREAK     0x9D
-#define ENTER           0x1C
+#include "keyboard.h"
 
 __attribute__((interrupt)) void div_by_0_handler(int_frame_32_t *frame) {
         kprintf("DIVIDE BY 0 ERROR\r\n");
@@ -24,15 +16,16 @@ __attribute__((interrupt)) void div_by_0_handler(int_frame_32_t *frame) {
 __attribute__((interrupt)) void keyboard_handler(int_frame_32_t *frame) {
         uint8_t key = inb(0x60);        // This is the port that the key data goes into (input)
                                         // For mouse, we need to use port 64, and then put a certain hex code into AL, I believe.
-
         if(key & 0x80) {
                 // nothing for now
         }
         else {
+                char keymapped = keymap[key];
                 if(key == ENTER) {
-                        kprintf("\r\n");
+                        // for now, we don't do anything, but eventually we'll want to save whatever the user types.
+                        // idk how to do that just yet, im guessing i'll need a global buffer of some sort or something idk lol
                 }
-                kprintf("key pressed: 0x%x\r\n", key);
+                kprintf("%c", keymapped);
         }
         PIC_sendEOI(1);        // We tell the PIC that we're done with processing this interrupt
 }
