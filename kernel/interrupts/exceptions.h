03eb0d555eb5513cacee6069c9ad1050bd465f5f
crimlegfish <angadbhatia122@gmail.com>
2025-06-25 14:28:43 -0500

    Update exceptions.h

--- kernel/interrupts/exceptions.h
+++ kernel/interrupts/exceptions.h
@@ -3,6 +3,8 @@
 extern char input_buffer[256];
 extern unsigned int input_pos;
 extern bool input_ready;
+extern volatile ticks;
 
 void div_by_0_handler(int_frame_32_t *frame);
 void keyboard_handler(int_frame_32_t *frame);
+void PIT_handler(int_frame_32_t *frame);
