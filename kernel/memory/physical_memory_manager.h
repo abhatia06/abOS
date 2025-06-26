74daf275e4f0a0246bce06f887d81352e980b86e
crimlegfish <angadbhatia122@gmail.com>
2025-06-26 14:11:00 -0500

    Update physical_memory_manager.h

--- kernel/memory/physical_memory_manager.h
+++ kernel/memory/physical_memory_manager.h
@@ -13,3 +13,5 @@ void unset_block(uint32_t bit);
 int32_t find_free_blocks(uint32_t num_blocks);
 void* allocate_blocks(uint32_t num_blocks);
 void free_blocks(uint32_t num_blocks);
+void deinit_memory_region(uint32_t base_address, uint32_t size);
+void initialize_memory_region(uint32_t base_address, uint32_t size);
