4b4e66d29b9c12570bf9c3315ea33a3e000e9bf1
crimlegfish <angadbhatia122@gmail.com>
2025-06-29 16:01:26 -0500

    Update boot2.s

--- bootloader/boot2.s
+++ bootloader/boot2.s
@@ -2,7 +2,7 @@ BITS 32
 ORG 0x8000
 
 MOV ESI, 0x10000
-MOV EDI, 0x100000
+MOV EDI, 0x50000  ; originally, I was loading it to 0x100000 (1MB), but that is where the higher half kernel will be (in physical memory)
 MOV ECX, 4096      ; WHY was I just copying 512 bytes?????
 CLD
 REP MOVSB
