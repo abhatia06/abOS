2f239b2e6e801e06cea329930f7d885b3575da25
crimlegfish <angadbhatia122@gmail.com>
2025-06-30 15:00:11 -0500

    Add files via upload

---
+++ kernel/kernelHigher.c
@@ -0,0 +1,26 @@
+#include "stdint.h"
+#include "stdio.h"
+#include "interrupts/idt.h"
+#include "interrupts/pic.h"
+#include "interrupts/exceptions.h"
+#include "memory/physical_memory_manager.h"
+#include "util/string.h"
+#include "memory/virtual_memory_manager.h"
+
+void kernel_main() { 
+	volatile int *ptr = (int*)0xB8000;
+	*ptr = 0x2F4B;
+	while(1);
+	/*
+        kprintf("Higher kernel loaded!");
+	while(true) { 
+		char* command = readline();
+                if(strcmp((const char*)command, "printmem") == 0) {
+                        kprintf("\r\n");
+                        print_memmap_command();
+                        kprintf("\r\n");
+                }
+        }
+	*/
+
+}
