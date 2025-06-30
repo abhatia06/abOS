67fbdec4380fa63edcb9ea28f546dd6311251c86
crimlegfish <angadbhatia122@gmail.com>
2025-06-30 15:01:58 -0500

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,43 +4,58 @@
 #include "interrupts/pic.h"
 #include "interrupts/exceptions.h"
 #include "memory/physical_memory_manager.h"
+#include "util/string.h"
+#include "memory/virtual_memory_manager.h"
 
-void memorysetup() {	// Right now the function is really only for testing purposes, but it does seem to work, so that's nice.
+void memorysetup() {
         initialize_pmm();
-        initialize_memory_region(0x100000, 4096);
-        int32_t checker = find_free_blocks(1);
-        kprintf("bit address: %d\r\n", checker);
+        //deinit_memory_region(0x0, 0x12000);
+        deinit_memory_region(0x100000, 81920);  // Reserve 20 blocks of physical memory for the kernel
+        deinit_memory_region(0x7EE0000, 20480);
+        //initialize_memory_region(0x100000, 4096);
+        //int32_t checker = find_free_blocks(1);
+        //kprintf("bit address: %d\r\n", checker);
+        // Add this to inspect the stack
+        unsigned long esp;
+        __asm__ volatile ("mov %%esp, %0" : "=r"(esp));
+        kprintf("ESP: 0x%x\r\n", esp);
+
 }
 
 void main() {
-	// I moved this up here because, somehow, interrupts were being fired as soon as the IDT was being initialized, leading to a double fault. So, I moved it up here
-	pic_disable();	// temporarily stop listening to interrupts (mask all interrupts coming in). 
-	memorysetup();
-	print_memory_map();
-	initialize_vmm();	// Initialize the VMM
-	kprintf("\r\n");
-	initIDT();	// Set up the IDT
-	idt_set_descriptor(0, (uint32_t)div_by_0_handler, 0x8E);	// 0x8E is the interrupt gate flag. Others might use trap gate, but idc rn
-	idt_set_descriptor(0x0E, (uint32_t)page_fault_handler, 0x8E); 	// page fault handler is interrupt 14, and it sends an error code 
-	PIC_remap(0x20) // Look at the table, all external interrupts begin at 0x20. 
+        pic_disable();
+        memorysetup();
 
-	idt_set_descriptor(0x20, PIT_handler, 0x8E);
-	idt_set_descriptor(0x21, (uint32_t)keyboard_handler, 0x8E);	// We can finally have a keyboard handler and accept user input!!
-	IRQ_clear_mask(0);	// We need to re-enable listening to interrupts from IRQ0, which is connected to the PIT channel 0
-	IRQ_clear_mask(1);	// We need to re-enable listening to interrupts from IRQ1, which is connected to the keyboard device
+        kprintf("\n");
+        print_physical_memory();
+        kprintf("\n");
+        initialize_vmm();
 
-	//Set default PIT timer IRQ rate to be about 1 millisecond (1193182hz/1193hz is roughly 1000 hz)
-	set_PIT(0, 2, 1193)
+        initIDT();
+        idt_set_descriptor(0, (uint32_t)div_by_0_handler, 0x8E);
+        PIC_remap(0x20);
 
-	//uint8_t div0test = 0/0;	// This SHOULD throw an interrupt (NOTE FROM THE FUTURE: it does)
-	
-	const char* not_really_far_string = "far string";
-	puts("Hello World from C!\n");
-	kprintf("Formatted %% %c %s %ls\n" 'a', "string", not_really_far_string);
-	kprintf("Formatted %d %i %x %p %o %hd %hi %hhu %hhd\n", 1234, -5678, 0xdead, 0xbeef, 012345, (short)27, (short)-42, (unsigned char)20, (signed char)-10);
-	kprintf("Formatted %ld %lx %lld %llx\n", -100000000l, 0xdeadbeeful, 10200300400ll, 0xdeadbeeffeebdaedull);
+        idt_set_descriptor(0x20, (uint32_t)PIT_handler, 0x8E);
+        idt_set_descriptor(0x21, (uint32_t)keyboard_handler, 0x8E);
+        idt_set_descriptor(0x0E, (uint32_t)page_fault_handler, 0x8E);
 
-	// This is a test to see if our VMM actually works:
+        IRQ_clear_mask(0); // Enable keyboard interrupts
+        IRQ_clear_mask(1); // Enable timer (PIT)
+
+        //Set default PIT timer IRQ rate to be about 1 millisecond
+        set_PIT(0, 2, 11931);
+        // Testing kprintf
+        const char* far_str = "far string";
+        kprintf("Formatted %% %c %s %s\r\n", 'a', "string", far_str);
+        kprintf("Test: %lld\r\n", 10200300400ll);
+        kprintf("Formatted %llx\r\n", 0xdeadbeeffeebdaedull);
+        kprintf("Formatted %d %i %x %p %o %hd %hi %hhu %hhd\r\n", 1234, -5678, 0xdead, 0xbeef, 012345, (short)27, (short)-42, (unsigned char)20, (signed char)-10);
+
+        // testing input buffer from keyboard interrupts
+        kprintf("Type your name: ");
+        char* name = readline();
+        kprintf("Hello, %s!\r\n", name);
+
         map_page((void*)0xB8000, (void*)0xC00B8000);
         volatile char* vid = (volatile char*)0xC00B8000;        // 0xB8000 should map to 0xC00B8000 now, so writing at
                                                                 // 0xC00B00 should write to 0xB8000 instead. Inside the
@@ -50,12 +65,22 @@ void main() {
                                                                 // will be PRESENT and WRITABLE). So, writing to
                                                                 // 0xC00B8000 MUST write to 0xB8000.
         vid[0] = 'X';
-        vid[1] = 0x07;
-	
-	kprintf("Type your name: ");
-	char* name = readline();
-	kprintf("Hello, %s!\n", name);
+        vid[1] = 0x0F;
 
-	//__asm__ volatile("iret");	This will ALSO throw an interrupt (interrupt 13, chcek the OSDev IDT to see what it does)
-	return;
+        /*
+        while(true) {
+                char* command = readline();
+                if(strcmp((const char*)command, "printmem") == 0) {
+                        kprintf("\r\n");
+                        print_memmap_command();
+                        kprintf("\r\n");
+                }
+        }
+        */
+
+        //uint32_t check = (uint32_t *)0xC0000000;
+        //kprintf("First word printed at 0xC0000000 = 0x%x\n", check);
+
+
+        ((void (*)(void))0xC0000000)(); // jump to higher kernel 
 }
