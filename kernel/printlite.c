ccd7eab9ca2a06b038c0c27ecf0a7e3f62ed8373
crimlegfish <angadbhatia122@gmail.com>
2025-07-01 17:13:19 -0500

    Added printlite
    These files... Are probably unnecessary. I could've likely compiled stdio.c twice, and had it exist in both linkers, but.. Who cares, it's not that big of an issue.

---
+++ kernel/printlite.c
@@ -0,0 +1,54 @@
+// This is basically a copy of stdio.c and stdio.h, but it is a "lite" version for the prekernel to use. It has very basic and limited
+// functionality, hence why it is called "lite". 
+
+#include "stdint.h"
+#include "printlite.h"
+
+const unsigned WIDTH = 80;
+const unsigned HEIGHT = 25;
+const uint8_t DEFAULT_COLOR = 0x07;
+
+uint8_t* screenBuffer = (uint8_t*)0xB8000;
+int screenX = 0, screenY = 0;
+
+void printchar(int x, int y, char c) {
+	screenBuffer[2 * (y * WIDTH + x)] = c;
+}
+
+void colorassign(int x, int y, uint8_t color) {
+	screenBuffer[2 * (y * WIDTH + x) + 1] = color;
+}
+
+void clear() {
+        for(int y = 0; y < HEIGHT; y++) {
+                for(int x = 0; x < WIDTH; x++) {
+                        printchar(x, y, '\0');
+                        colorassign(x, y, DEFAULT_COLOR);
+                }
+        }
+
+        screenX = 0;
+        screenY = 0;
+}
+
+void printC(char c) {
+        switch(c) {
+                case '\n' :
+                        screenX = 0;
+                        screenY++;
+                        break;
+
+		default :
+			printchar(screenX, screenY, c);
+		        screenX++;
+			break;
+	}
+}
+
+void printS(const char* str) {
+	while(*str) {
+		printC(*str);
+		str++;
+	}
+}
+
---
+++ kernel/printlite.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void printS(const char* str);
+void printC(char c);
+void clear();
