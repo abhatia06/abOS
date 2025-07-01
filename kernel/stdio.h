b16c34e9d402a48825e652676589e3040f37dbf8
crimlegfish <angadbhatia122@gmail.com>
2025-06-30 22:39:41 -0500

    Update stdio.h

--- kernel/stdio.h
+++ kernel/stdio.h
@@ -1,5 +1,11 @@
 #pragma once
 
+void clrscr();
 void putc(char c);
 void puts(const char* str);
+void putchar(int x, int y, char c);
 void kprintf(const char* fmt, ...);
+char* readline();
+int get_cursor_pos();
+void print_physical_memory();
+void print_memmap_command();
