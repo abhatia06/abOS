b66f6ea3bed5ca902c061e7532ed27fa65c4ec29
crimlegfish <angadbhatia122@gmail.com>
2025-07-02 13:43:01 -0500

    Update global_addresses.h

--- kernel/global_addresses.h
+++ kernel/global_addresses.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #define CURRENT_PAGE_DIR_ADDRESS  0x1800
-#define MEMMAP_AREA     0x7EE0000
+#define MEMMAP_AREA     0x30000
 #define MAX_BLOCKS      0x1808
 #define USED_BLOCKS     0x1804
 #define TOTAL_MEMORY    0x180C
