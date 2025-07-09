7f76b85ca5f6e66ed1fac8be52c32c46ef5d4c9e
crimlegfish <angadbhatia122@gmail.com>
2025-07-08 19:37:11 -0500

    Update syscalls.h

--- kernel/interrupts/syscalls.h
+++ kernel/interrupts/syscalls.h
@@ -3,11 +3,13 @@
 
 #pragma once
 
-#define MAX_SYSCALLS 2
+#define MAX_SYSCALLS 4
 
 void syscall_handler();
 
 void sys_test1();
 void sys_test2();
+void sys_malloc();
+void sys_free();
 
 extern void* syscalls[MAX_SYSCALLS];
