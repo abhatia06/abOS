c889636f354f5fab0213789b8f2321e6b700fbdb
crimlegfish <angadbhatia122@gmail.com>
2025-07-08 19:36:44 -0500

    Update syscalls.c

--- kernel/interrupts/syscalls.c
+++ kernel/interrupts/syscalls.c
@@ -1,56 +1,71 @@
 #include "syscalls.h"
 #include "../stdio.h"
 #include "../stdint.h"
+#include "../stdlib.h"
+
 
 // The main system call will be int 0x80
 
 void sys_test1() {
-	kprintf("Test syscall 1\n");
+        kprintf("Test syscall 1\n");
 }
 
 
 void sys_test2() {
-	kprintf("Test syscall 2\n");
+        kprintf("Test syscall 2\n");
+}
+
+void sys_malloc() {
+        uint32_t bytes;
+
+        __asm__ volatile("mov %%EBX, %0" : "=b"(bytes));
+}
+
+void sys_free() {
+
 }
 
 void* syscalls[MAX_SYSCALLS] = {
-	sys_test1,
-	sys_test2,
+        sys_test1,
+        sys_test2,
+        sys_malloc,
+        sys_free,
 };
 
-__attribute__((naked)) 	void syscall_handler() {
-	__asm__ volatile (".intel_syntax noprefix\n"
+__attribute__((naked))  void syscall_handler() {
+        __asm__ volatile (".intel_syntax noprefix\n"
 
-			  ".equ MAX_SYSCALLS, 2\n"
-			  "cmp eax, MAX_SYSCALLS-1\n"
-			  "ja invalid_syscall\n"
+                          ".equ MAX_SYSCALLS, 4\n"
+                          "cmp eax, MAX_SYSCALLS-1\n"
+                          "ja invalid_syscall\n"
 
-			  "push gs\n"
-			  "push fs\n"
-			  "push es\n"
-			  "push ds\n"
-			  "push ebp\n"
-			  "push edi\n"
-			  "push esi\n"
-			  "push edx\n"
-			  "push ecx\n"
-			  "push ebx\n"
-			  "push esp\n"
-			  "call [syscalls+eax*4]\n"
-			  "add esp, 4\n"
-			  "pop ebx\n"
-			  "pop ecx\n"
-			  "pop edx\n"
-			  "pop esi\n"
-			  "pop edi\n"
-			  "pop ebp\n"
-			  "pop ds\n"
-			  "pop es\n"
-			  "pop fs\n"
-			  "pop gs\n"
-			  
-			  "iretd\n"
+                          "push gs\n"
+                          "push fs\n"
+                          "push es\n"
+                          "push ds\n"
+                          "push ebp\n"
+                          "push edi\n"
+                          "push esi\n"
+                          "push edx\n"
+                          "push ecx\n"
+                          "push ebx\n"
+                          "push esp\n"
+                          "call [syscalls+eax*4]\n"
+                          "add esp, 4\n"
+                          "pop ebx\n"
+                          "pop ecx\n"
+                          "pop edx\n"
+                          "pop esi\n"
+                          "pop edi\n"
+                          "pop ebp\n"
+                          "pop ds\n"
+                          "pop es\n"
+                          "pop fs\n"
+                          "pop gs\n"
 
-			  "invalid_syscall:\n"
-			  "iretd\n");
+                          "iretd\n"
+
+                          "invalid_syscall:\n"
+                          "mov eax, -1\n"
+                          "iretd\n");
 }
