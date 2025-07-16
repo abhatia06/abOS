4573236b662a19ec2a05160a63ed8fd0e166b863
crimlegfish <angadbhatia122@gmail.com>
2025-07-16 13:14:44 -0500

    Update stdlib.c

--- kernel/stdlib.c
+++ kernel/stdlib.c
@@ -1,6 +1,14 @@
 #include "stdint.h"
 #include "stdlib.h"
 
+/*
+ * I SHOULD PUT A DISCLAIMER HERE:
+ * technically, these are WRONG. Malloc and free are NOT supposed to be syscalls, instead, they should be part of the stdlib
+ * library (which they are), and they should be available for the user to use. BUT, for right now, I am not doing that. Currently,
+ * I only care about my implementation of malloc WORKING. Eventually, I will move it over to be existing in the user space, and
+ * then have sbrk and brk be the only syscalls it executes.
+ */
+
 void* malloc(uint32_t size) {
 	void* ptr;
 
