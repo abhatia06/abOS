32baca4e2990f9d5f632f0df8b69b1dcb88cfca1
crimlegfish <angadbhatia122@gmail.com>
2025-07-16 11:25:22 -0500

    Add files via upload

---
+++ kernel/memory/malloc.c
@@ -0,0 +1,26 @@
+#include "malloc.h"
+#include "../stdint.h"
+#include "../global_addresses.h"
+
+
+#define NULL 0
+
+void* malloc_init() {
+	
+}
+
+void* malloc(uint32_t size) {
+	return NULL;
+}
+
+void* calloc(uint32_t size) {
+	return NULL;
+}
+
+void* split_blocks(uint32_t size) {
+	return NULL;
+}
+
+void merge_Free_blocks() {
+
+}
---
+++ kernel/memory/malloc.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../stdint.h"
+#include "../global_addresses.h"
+
+#define PAGE_SIZE 4096
+
+typedef struct malloc_node {
+	uint32_t size;
+	bool free;
+	struct malloc_node* next;
+	void* address;		// maybe? I might remove this later
+} malloc_node_t;
+
+// We will be using a linked list approach for malloc
+malloc_node_t* malloc_head;
+uint32_t malloc_virt_address;
+uint32_t malloc_phys_address;
+uint32_t total_malloc_pages;
+uint32_t malloc_start;
+
+void* malloc_init();
+void* malloc(uint32_t size);
+void* calloc(uint32_t size);
+void* split_blocks(uint32_t size);
+void merge_free_blocks();
