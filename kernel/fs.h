7a85733c535bd7853845e6f199711c657f918f44
crimlegfish <angadbhatia122@gmail.com>
2025-07-18 21:51:22 -0500

    fs bones

---
+++ kernel/fs.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "stdint.h"
+
+typedef struct inode_block {
+
+} inode_block_t;
+
+typedef struct super_block {
+
+} super_block_t;
+
+typedef struct data_block {
+
+} data_block_t;
+
+extern uint32_t inode_bitmap;
+extern uint32_t data_bitmap;
+
+
