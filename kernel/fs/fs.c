cbf81322554084577a41a6e1008ecd7285d5116d
crimlegfish <angadbhatia122@gmail.com>
2025-07-21 20:23:27 -0500

    Add files via upload

---
+++ kernel/fs/fs.c
@@ -0,0 +1,55 @@
+#include "fs.h"
+#include "../stdint.h"
+#include "../stdio.h"
+#include "../x86.h"
+
+#define READ 0x20
+#define WRITE 0x30
+
+void rw_sectors(uint32_t sectors, uint32_t starting_sector, uint32_t address, int readwrite) {
+	outb(0x1F6, (0xE0 | ((starting_sector >> 24) & 0x0F)));
+	outb(0x1F2, sectors);
+	outb(0x1F3, ((starting_sector >> 8) & 0xFF));
+	outb(0x1F4, ((starting_sector >> 16) & 0xFF));
+	outb(0x1F7, readwrite);
+
+	uint16_t* address_ptr = (uint16_t*)address;
+	if(readwrite == READ) {
+		for(uint32_t i = 0; i < sectors; i++) {
+			while(inb(0x1F7) & (1 << 7)) {
+				// poll
+			}
+
+			for(uint32_t j = 0; j < 512; j++) {
+				*address_ptr++ = inb(0x1F0);
+			}
+
+			// 400ns delay
+			for(int z = 0; z < 4; z++) {
+				inb(0x3F6);
+			}
+		}
+	}
+
+	else if(readwrite == WRITE) {
+		for(uint32_t i = 0; i < sectors; i++) {
+			while(inb(0x1F7) & (1 << 7)) {
+				// poll
+			}
+
+			for(uint32_t j = 0; j < 512; j++) {
+				outb(0x1F0, *address_ptr++);
+			}
+
+			for(int z = 0; z < 4; z++) {
+				inb(0x3F6);
+			}
+		}
+
+		outb(0x1F7, 0xE7);
+		while(inb(0x1F7) & (1 << 7)) {
+			//poll
+		}
+	}
+}	
+
