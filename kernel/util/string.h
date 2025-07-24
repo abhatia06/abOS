5e473f71c488fc9b0d152272dbdb5f81de991a61
crimlegfish <angadbhatia122@gmail.com>
2025-07-24 07:51:09 -0500

    Update string.h

--- kernel/util/string.h
+++ kernel/util/string.h
@@ -4,4 +4,4 @@ int strcmp(const char* str1, const char* str2);
 long int strlen(const char* str);
 char* strrchr(char* str, int c);
 void* memset(void* ptr, uint8_t value, uint32_t length);
-char* strncpy(char* dst, const char* src, uint32_t n)
+char* strncpy(char* dst, const char* src, uint32_t n);
