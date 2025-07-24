7607f25d1fb8f789256d88d77b74fffb163fa4e9
crimlegfish <angadbhatia122@gmail.com>
2025-07-23 21:29:28 -0500

    Update fs.h

--- kernel/fs/fs.h
+++ kernel/fs/fs.h
@@ -47,6 +47,8 @@ typedef struct super_block {
         uint32_t num_inodes_per_sector;
         uint32_t first_inode_block;        // stores the first block #, (NOT byte address OR sector #)
         uint32_t first_data_block;        // stores the first block #, (NOT byte address OR sector #)
+        uint32_t first_inode_bitmap_block;
+        uint32_t first_data_bitmap_block;
         uint32_t inode_size;            // likely going to be 128 bytes?
         uint32_t num_datablocks;
         uint32_t num_datablocks_bitmap;
@@ -55,7 +57,7 @@ typedef struct super_block {
         uint32_t root_i_number;         // using inode_block() and inode_sector() functions can give us the root inode 
         uint32_t first_free_inode_bit;  // stole this off queso fuego (originally was gonna make a separate function)
         uint32_t first_free_data_bit;
-        uint8_t padding[36]  
+        uint8_t padding[28]  
 } super_block_t;                // CURRENT SIZE: 128 BYTES
 
 // everything below is for the files & directories
