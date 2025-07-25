bde37051eff188e067d50c24c691b8196ffcc65e
crimlegfish <angadbhatia122@gmail.com>
2025-07-25 13:26:01 -0500

    Update fs.h

--- kernel/fs/fs.h
+++ kernel/fs/fs.h
@@ -57,7 +57,7 @@ typedef struct super_block {
         uint32_t root_i_number;         // using inode_block() and inode_sector() functions can give us the root inode 
         uint32_t first_free_inode_bit;  // stole this off queso fuego (originally was gonna make a separate function)
         uint32_t first_free_data_bit;
-        uint8_t padding[28]  
+        uint8_t padding[60]  
 } super_block_t;                // CURRENT SIZE: 128 BYTES
 
 // everything below is for the files & directories
