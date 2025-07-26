796d6f4d31b129c576f3cb43bdb0a3daf4e0b282
crimlegfish <angadbhatia122@gmail.com>
2025-07-25 19:47:50 -0500

    Update format_disk.c

--- format_disk.c
+++ format_disk.c
@@ -13,7 +13,42 @@
 // Initialize/build inode block
 // Initialize/build data blocks
 
-int main() {
+typedef struct {
+        char path[60];
+        uint32_t size;
+        FILE *file;
+} file_pointer;
 
-	return 0;
+file_pointer files[] = {
+        {"build/boot.bin", 0, NULL},
+        {"build/bootstage2.bin", 0, NULL},
+        {"build/prekernel.bin", 0, NULL},
+        {"build/kernel.bin", 0, NULL},
+};
+
+superblock_t superblock = (superblock_t){0};
+uint32_t disk_size = 512*2880;  // keeping it to be 1.44MB for right now
+uint32_t num_files = sizeof(files)/sizeof(files[0]);    // total bytes of array/bytes per element = size of array
+
+bool write_boot_block() {
+        return false;
+}
+
+bool write_superblock() {
+        return false;
+}
+
+
+int main() {
+	
+	// correct file attributes 
+        for(uint32_t i = 0; i < num_files; i++) {
+                files[i].file = fopen(files[i].path, "rb");     //open file (get pointer)
+
+                fseek(files[i].file, 0, SEEK_END);              //set pointer to end of the file
+                files[i].size = ftell(files[i].file);           //get current pos of pointer (which is the same as the size then)
+		fclose(files[i].file);
+        }
+	
+        return 0;
 }
