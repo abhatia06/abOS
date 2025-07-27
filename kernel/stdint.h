76f0bce7a26c94dca837a346aed8b47be243519e
crimlegfish <angadbhatia122@gmail.com>
2025-07-26 23:48:18 -0500

    Update stdint.h

--- kernel/stdint.h
+++ kernel/stdint.h
@@ -4,10 +4,10 @@ typedef signed char int8_t;
 typedef unsigned char uint8_t;
 typedef signed short int16_t;
 typedef unsigned short uint16_t;
-typedef signed long int int32_t;
-typedef unsigned long int uint32_t;
-typedef signed long long int int64_t;
-typedef unsigned long long int uint64_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;  // int is usually 32-bits (https://learn.microsoft.com/en-us/cpp/c-language/type-int?view=msvc-170)
+typedef signed long int int64_t;    // 32-bit uses long long int to define 64-bit, because long int not long enough. 64-bit uses long int
+typedef unsigned long long int uint64_t; // for some reason, 64-bit also uses long long int for uint64_t????? I don't really get why but ok ig
 
 
 typedef uint8_t bool;
