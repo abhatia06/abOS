cb8c405bd205325dc0015e6740ebd94fd1074bf7
crimlegfish <angadbhatia122@gmail.com>
2025-07-30 13:29:53 -0500

    Update fs.h

--- kernel/fs/fs.h
+++ kernel/fs/fs.h
@@ -36,6 +36,7 @@ typedef struct inode {
         fs_time_t time_created;
         uint32_t direct_pointers[4];            // 4 direct pointers, mostly because the kernel needs 4 blocks (will likely change to 5 soon)
         uint32_t single_indirect_block;         // 1 single indirect pointer
+        uint8_t padding[11];
 } inode_t;        // 64 bytes
 
 typedef struct super_block {
