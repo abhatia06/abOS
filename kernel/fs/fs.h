a1aeb1a46c01fc0c124e70dee51b64cee4d5ebab
crimlegfish <angadbhatia122@gmail.com>
2025-07-31 16:09:58 -0500

    Update fs.h

--- kernel/fs/fs.h
+++ kernel/fs/fs.h
@@ -84,26 +84,3 @@ typedef struct dir_entry_t {
 #define INODES_PER_SECTOR (FS_SECTOR/sizeof(inode_t))
 #define INODES_PER_BLOCK (FS_BLOCK/sizeof(inode_t))
 #define DIR_ENTRIES_PER_BLOCK (FS_BLOCK/sizeof(dir_entry_t))
-
-extern inode_t current_dir_inode;
-extern superblock_t superblock;
-extern int block_buffer[FS_BLOCK];
-extern int sector_buffer[FS_SECTOR];
-extern inode_t root_inode;
-
-
-void rw_sectors(uint32_t sectors, uint32_t starting_sector, uint32_t address, int readwrite);
-bool load_file(inode_t* inode, uint32_t address);
-bool save_file(inode_t* node, uint32_t address);
-inode_t get_inode_in_dir(inode_t current_dir, char* file);
-inode_t get_inode(char* path);
-inode_t get_parent_inode(char* path);
-void set_inode_bitmap(uint32_t bit, bool set);
-void set_data_bitmap(uint32_t bit, bool set);
-char* get_name_path(char* path);
-void update_superblock();
-void update_inode(inode_t inode);
-uint32_t find_free_bit(uint32_t start_block, uint32_t length_blocks);
-inode_t create_file(char* filepath);
-bool print_dir(char* path);
-bool delete_file(inode_t* inode);
