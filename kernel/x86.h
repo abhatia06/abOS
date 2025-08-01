f328b841b44f96e1afa6cbf4047d763edfded1c0
crimlegfish <angadbhatia122@gmail.com>
2025-07-31 20:29:27 -0500

    Update x86.h

--- kernel/x86.h
+++ kernel/x86.h
@@ -2,8 +2,10 @@
 
 #include "stdint.h"
 
-void _x86_div64_32(uint64_t dividend, uint32_t divisor, uint64_t* quotient, uint32_t* remainderOut);
+void _x86_div64_32(uint64_t number, uint32_t divisor, uint64_t* quotient, uint32_t* remainderOut);
 void _x86_Video_WriteCharTeletype(char c, uint8_t page, uint8_t pos);
-void _x86_outb(uint16_t port, uint8_t value);
-uint8_t _x86_inb(uint16_t port);
+void outb(uint16_t port, uint8_t value);
+uint8_t inb(uint16_t port);
+void outw(uint16_t port, uint16_t value);
+uint16_t inw(uint16_t port);
 void io_wait();
