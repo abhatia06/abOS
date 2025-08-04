eb22cbcc0df9893ed5d13a6c9317cdb205a82cc0
crimlegfish <angadbhatia122@gmail.com>
2025-08-04 11:43:15 -0500

    Update prekernel.c

--- kernel/prekernel.c
+++ kernel/prekernel.c
@@ -3,16 +3,16 @@
 #include "util/string.h"
 #include "stdint.h"
 #include "global_addresses.h"
-#include "printlite.h"
+#include "stdio.h"
 
 
 __attribute__ ((section ("prekernel_entry"))) void prekernel_main() {
         // For some reason, the prekernel setup is INCREDIBLY slow. I don't know why, I am guessing it's due to the SMAP stuff?
 
         asm volatile ("ltr %0" :: "r"(0x28));
-        printS("Successfully entered Protected Mode!\n");
-        printS("Successfully entered Lower-Memory Kernel!\n");
-        printS("Initializing Physical Memory Manager...\n");
+        kprintf("Successfully entered Protected Mode!\n");
+        kprintf("Successfully entered Lower-Memory Kernel!\n");
+        kprintf("Initializing Physical Memory Manager...\n");
         initialize_pmm();        //TODO: initializing pmm takes like 10-15 seconds. Fix unoptimization in PMM
 
         typedef struct SMAP_entry {
@@ -45,13 +45,13 @@ __attribute__ ((section ("prekernel_entry"))) void prekernel_main() {
         deinit_memory_region(MEMMAP_AREA, max_blocks/BLOCK_SIZE);
         deinit_memory_region(0x100000, 81920);  // Deinitialize some of the physical memory at 1MB, because that is for kernel
 
-        printS("Initialized Physical Memory Manager!\n");
+        kprintf("Initialized Physical Memory Manager!\n");
 
-        printS("Initializing Virtual Memory Manager & Enabling Paging...\n");
+        kprintf("Initializing Virtual Memory Manager & Enabling Paging...\n");
         initialize_vmm();       // Initialize VMM
 
-        printS("Initialized Virtual Memory Manager!\n");
-        printS("Enabled Paging!\n");
+        kprintf("Initialized Virtual Memory Manager!\n");
+        kprintf("Enabled Paging!\n");
 
         // managed to get this to work
         for(uint32_t virt = 0x100000; virt < 0x400000; virt += PAGE_SIZE) {
@@ -66,11 +66,11 @@ __attribute__ ((section ("prekernel_entry"))) void prekernel_main() {
 
         *(uint32_t *)CURRENT_PAGE_DIR_ADDRESS = (uint32_t)directory;
         //*(uint32_t *)CURRENT_PD_ADD = (uint32_t)current_pd_address;   // already a uint32_t, but whatever
-        printS("Pre-kernel setup done! Getting ready to jump to virtual address 0xC0000000\n");
+        kprintf("Pre-kernel setup done! Getting ready to jump to virtual address 0xC0000000\n");
 
         //map_page((void*)0x200000, (void*)0xBFFFF000);
         //map_page((void*)0x500000, (void*)0x500000);        //these are for testing. Will remove eventually
-        clear();
+        clrscr();
         // Jump to 0xC0000000 (which is mapped to 0x100000). (void (*)(void)) is a function pointer cast (void ret type & void args)
         ((void (*)(void))0xC0000000)();
 
