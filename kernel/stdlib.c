b0bda425204329582214229a017f609ca657086b
crimlegfish <angadbhatia122@gmail.com>
2025-08-05 17:29:57 -0500

    Update stdlib.c

--- kernel/stdlib.c
+++ kernel/stdlib.c
@@ -12,7 +12,7 @@
 void* malloc(uint32_t size) {
 	void* ptr;
 
-	__asm__ volatile("int $0x80" : : "a"(3), "b"(size));
+	__asm__ volatile("int $0x80" : : "a"(2), "b"(size));
 	__asm__ volatile("movl %%eax, %0" : "=r"(ptr));
 
 	return ptr;  
@@ -20,7 +20,7 @@ void* malloc(uint32_t size) {
 
 void free(void *ptr) {
 	// the 4th syscall can handle it all lol
-	__asm__ volatile("int $0x80" : : "a"(4), "b"(ptr));
+	__asm__ volatile("int $0x80" : : "a"(3), "b"(ptr));
 }	
 
 
