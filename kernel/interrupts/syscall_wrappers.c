0db9e41a871d7aa63ee00672d44646fabdb81247
crimlegfish <angadbhatia122@gmail.com>
2025-08-05 20:46:47 -0500

    Update syscall_wrappers.c

--- kernel/interrupts/syscall_wrappers.c
+++ kernel/interrupts/syscall_wrappers.c
@@ -17,3 +17,23 @@ int32_t close(int32_t file_descriptor) {
 
 	return result;
 }
+
+// also might change in the future? Might not require a buffer, who knows. I'll see.
+// if we had like stdout, stderr, and stdin, if we wanted to be able to print to screen, we would call write() with
+// fd = 1, or 0, or 2, whatever fd we assign stdout to. If an error popped up, it would write to stderr, and if we
+// wanted to input a file, we'd use stdin. But uh. I don't really wanna deal with the standard stream stuff just yet
+int32_t write(int32_t file_descriptor, void* buffer, uint32_t length) {
+        int32_t result = -1;
+
+        __asm__ volatile("int $0x80" : "=a"(result) : "a"(6), "b"(file_descriptor), "c"(buffer), "d"(length) : "memory");
+        return result;
+}
+
+// read is supposed to read the contents of a file and write to a buffer, and then we can use that buffer for other syscalls (write)
+int32_t read(int32_t file_descriptor, void* buffer, uint32_t length) {
+        int32_t result = -1;
+
+        __asm__ volatile("int $0x80" : "=a"(result) : "a"(7), "b"(file_descriptor), "c"(buffer), "d"(length) : "memory");
+	
+        return result;
+}
