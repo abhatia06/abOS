3727ea247471bd5aa3b3fc262e5b6110f17b5952
abhatia06 <angadbhatia122@gmail.com>
2025-09-28 23:11:01 -0500

    Update physical_memory_manager.h

--- kernel/memory/physical_memory_manager.h
+++ kernel/memory/physical_memory_manager.h
@@ -1,8 +1,10 @@
 #pragma once
 
-#define FRAME_SIZE	4096 // generally paging has each frame be 4KB
-#define BITMAP_DIVISON 	8    // Memory is already divided into partitions of 8 bits (or 1 byte), so this is easier
-#define start_addr      0x7EE0000  // Close to the end of available memory for us
+#include "../stdint.h"
+
+#define BLOCK_SIZE      4096 // generally paging has each frame be 4KB
+#define BITMAP_DIVISON  8    // Memory is already divided into partitions of 8 bits (or 1 byte), so this is easier
+#define start_addr      0x30000  // decided to place it at hexadecimal 30000. It won't be reachable by the user!
 
 extern uint32_t *memory_map;
 extern uint32_t max_blocks;
@@ -10,9 +12,9 @@ extern uint32_t used_blocks;
 
 void set_block(uint32_t bit);
 void unset_block(uint32_t bit);
-int32_t find_free_blocks(uint32_t num_blocks);
-void* allocate_blocks(uint32_t num_blocks);
-void free_blocks(uint32_t num_blocks);
 void deinit_memory_region(uint32_t base_address, uint32_t size);
 void initialize_memory_region(uint32_t base_address, uint32_t size);
+int32_t find_free_blocks(uint32_t num_blocks);
+void* allocate_blocks(uint32_t num_blocks);
+void free_blocks(uint32_t bit, uint32_t num_blocks);
 void initialize_pmm();
