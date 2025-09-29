66aa7b6d42d5a8e31b46d44e1b142c547c2970cf
abhatia06 <angadbhatia122@gmail.com>
2025-09-28 23:20:59 -0500

    Update stdlib.c

--- kernel/stdlib.c
+++ kernel/stdlib.c
@@ -1,14 +1,6 @@
 #include "stdint.h"
 //#include "stdlib.h"
 
-/*
- * I SHOULD PUT A DISCLAIMER HERE:
- * technically, these are WRONG. Malloc and free are NOT supposed to be syscalls, instead, they should be part of the stdlib
- * library (which they are), and they should be available for the user to use. BUT, for right now, I am not doing that. Currently,
- * I only care about my implementation of malloc WORKING. Eventually, I will move it over to be existing in the user space, and
- * then have sbrk and brk be the only syscalls it executes.
- */
-
 void testertesting123() {
         __asm__ volatile("int $0x80" : : "a"(1));
 }
