1b361ee2955866b5f591ff1e462806c4c678d504
abhatia06 <angadbhatia122@gmail.com>
2025-09-28 23:07:04 -0500

    Update malloc.h

--- kernel/memory/malloc.h
+++ kernel/memory/malloc.h
@@ -4,23 +4,24 @@
 #include "../global_addresses.h"
 
 #define PAGE_SIZE 4096
-#define NULL 0
+#define NULL0 0
 
 typedef struct malloc_node {
         uint32_t size;
         bool free;
         struct malloc_node* next;
         struct malloc_node* prev;
-        void* address;          // maybe? I might remove this later
+        void* address;                  // This might seem weird. Most malloc implementations don't have this, but I do because
+                                        // I'm a bad coder lol
 } malloc_node_t;
 
 // We will be using a linked list approach for malloc
-malloc_node_t* malloc_head;
-uint32_t malloc_virt_address;
-uint32_t malloc_phys_address;
-uint32_t total_malloc_pages;
-uint32_t malloc_start;
-uint32_t heap_end;
+extern malloc_node_t* malloc_head;
+extern uint32_t malloc_virt_address;
+extern uint32_t malloc_phys_address;
+extern uint32_t total_malloc_pages;
+extern uint32_t malloc_start;
+extern uint32_t heap_end;
 
 void* malloc_init();
 void* malloc_more_pages(uint32_t size);
