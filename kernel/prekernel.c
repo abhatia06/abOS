acfc604d8ef47ff7f9adc1a6928d3660cb759cc9
abhatia06 <angadbhatia122@gmail.com>
2025-09-28 23:16:46 -0500

    Update prekernel.c

--- kernel/prekernel.c
+++ kernel/prekernel.c
@@ -3,17 +3,17 @@
 #include "util/string.h"
 #include "stdint.h"
 #include "global_addresses.h"
+#include "printlite.h"
+#include "fs/fs.h"
 #include "stdio.h"
-
 
 __attribute__ ((section ("prekernel_entry"))) void prekernel_main() {
         // For some reason, the prekernel setup is INCREDIBLY slow. I don't know why, I am guessing it's due to the SMAP stuff?
-
         asm volatile ("ltr %0" :: "r"(0x28));
         kprintf("Successfully entered Protected Mode!\n");
         kprintf("Successfully entered Lower-Memory Kernel!\n");
         kprintf("Initializing Physical Memory Manager...\n");
-        initialize_pmm();        //TODO: initializing pmm takes like 10-15 seconds. Fix unoptimization in PMM
+        initialize_pmm();
 
         typedef struct SMAP_entry {
                 uint64_t base_address;
@@ -38,14 +38,20 @@ __attribute__ ((section ("prekernel_entry"))) void prekernel_main() {
         //*(uint32_t*)USED_BLOCKS = used_blocks;
         //*(uint32_t*)MAX_BLOCKS = max_blocks;
 
-        // These were part of the issue I was having. Apparently, since we weren't de-initializing the regions that house our OS stuff among other things, it was 
-        // causing issues? I don't fully know WHY it was causing issues, since, y'know, the PMM and the VMM don't actually load anything onto the addresses that are
-        // "mapped" or "allocated", (we still haven't made a malloc yet), but alright, I guess. I need to do more research to figure out why this was an issue.
         deinit_memory_region(0x0, 0x12000);
         deinit_memory_region(MEMMAP_AREA, max_blocks/BLOCK_SIZE);
         deinit_memory_region(0x100000, 81920);  // Deinitialize some of the physical memory at 1MB, because that is for kernel
 
+        //printS("Loading the root directory...\n");
+
+        //superblock = *(superblock_t*)0x8C00;
+        //rw_sectors(1, superblock.first_inode_block*8, (uint32_t)sector_buffer, READ);
+        //root_inode = *((inode_t*)sector_buffer + 1);
+        //superblock.root_i_number = (uint32_t)&root_inode;
+        //current_dir_inode = root_inode;
+
         kprintf("Initialized Physical Memory Manager!\n");
+        *(uint32_t *)CURRENT_PAGE_DIR_ADDRESS = (uint32_t)directory;
 
         kprintf("Initializing Virtual Memory Manager & Enabling Paging...\n");
         initialize_vmm();       // Initialize VMM
@@ -53,7 +59,7 @@ __attribute__ ((section ("prekernel_entry"))) void prekernel_main() {
         kprintf("Initialized Virtual Memory Manager!\n");
         kprintf("Enabled Paging!\n");
 
-        // managed to get this to work
+
         for(uint32_t virt = 0x100000; virt < 0x400000; virt += PAGE_SIZE) {
                 unmap_page((void *)virt);
         }
@@ -68,11 +74,18 @@ __attribute__ ((section ("prekernel_entry"))) void prekernel_main() {
         //*(uint32_t *)CURRENT_PD_ADD = (uint32_t)current_pd_address;   // already a uint32_t, but whatever
         kprintf("Pre-kernel setup done! Getting ready to jump to virtual address 0xC0000000\n");
 
+
         //map_page((void*)0x200000, (void*)0xBFFFF000);
-        //map_page((void*)0x500000, (void*)0x500000);        //these are for testing. Will remove eventually
+        //map_page((void*)0x500000, (void*)0x500000);
+
+        /*
+        while(true) {
+                __asm__ volatile("cli; hlt");
+        }
+        */
+
         clrscr();
         // Jump to 0xC0000000 (which is mapped to 0x100000). (void (*)(void)) is a function pointer cast (void ret type & void args)
         ((void (*)(void))0xC0000000)();
-
-
+        __asm__ volatile("cli;hlt" : : "a"(0xbeefdead));
 }
