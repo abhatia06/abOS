ad2d707192b658f95e937b2a7366783dd1b7d8c5
abhatia06 <angadbhatia122@gmail.com>
2025-09-28 23:10:01 -0500

    Update physical_memory_manager.c

--- kernel/memory/physical_memory_manager.c
+++ kernel/memory/physical_memory_manager.c
@@ -1,7 +1,10 @@
 #include "physical_memory_manager.h"
+#include "../stdint.h"
+#include <stdbool.h>
+#include "../util/string.h"
 
-uint32_t *memory_map = (uint32_t*)start_addr;
-uint32_t max_blocks = 0xFFFFFFFF/BLOCK_SIZE;    // Maximum number of available blocks
+uint32_t *memory_map = 0;
+uint32_t max_blocks = 0;                // Maximum number of available blocks
 uint32_t used_blocks = 0;
 
 void set_block(uint32_t bit) {
@@ -25,6 +28,29 @@ void unset_block(uint32_t bit) {
         memory_map[bit/32] &= ~(1 << (bit % 32));
 }
 
+/*        // I added this like a while ago... I don't remember what I used it for, but I don't think it's ever used. But, in the case it is, it's here.
+void unset_block_bulk(uint32_t start_bit, uint32_t count) {
+        uint32_t bit = start_bit;
+        uint32_t end_bit = start_bit + count;
+
+        while(bit < end_bit) {
+                uint32_t word_index = bit/32;
+                uint32_t bit_offset = bit%32;
+
+                if(bit_offset == 0 && (end_bit - bit) >= 32) {
+                        memory_map[word_index] = 0;
+                        bit += 32;
+                }
+                else {
+                        memory_map[word_index] &= ~(1 << bit_offset);
+                        bit++;
+                }
+        }
+
+        used_blocks -= count;
+}
+*/
+
 // This function is quite unoptimized. You will find better versions/make a better version yourself, but for now, I only
 // care about if it works or not. This function will scan every bit 1-by-1, so the performance is quite slow.
 int32_t find_free_blocks(uint32_t num_blocks) {
@@ -36,14 +62,14 @@ int32_t find_free_blocks(uint32_t num_blocks) {
         uint32_t holder = 0;
         for(uint32_t i = 0; i < max_blocks; i++) {
                 holder = memory_map[i/32];      // Holds the 32-bit integer inside index i/32
-                if(!(holder & (1 << (i%32)))) { // Check to see if the current bit is 0 (not mapped/unset)
+                if(!(holder & (1 << (i%32)))) { // Check to see if the current bit is 0
                         count++;
                         if(count == num_blocks) {
-                                return (i-num_blocks+1)*BLOCK_SIZE;     // Return starting address of available memory
+                                return (i-num_blocks+1);        // Return starting address of available memory
                         }
                 }
                 else {
-                        count = 0;        // If the current bit is 1, we should rese the count
+                        count = 0;
                 }
         }
         // Unable to find available memory
@@ -83,6 +109,10 @@ void initialize_memory_region(uint32_t base_address, uint32_t size) {
         int32_t align = base_address/BLOCK_SIZE;
         int32_t num_blocks = size/BLOCK_SIZE;
 
+        if(num_blocks == 0) {
+                return;
+        }
+        
         for(; num_blocks > 0; num_blocks--) {
                 unset_block(align++);
                 used_blocks--;
@@ -105,6 +135,10 @@ void deinit_memory_region(uint32_t base_address, uint32_t size) {
 }
 
 void initialize_pmm() {
+        memory_map = (uint32_t*)start_addr;
+        max_blocks = 0xFFFFFFFF/BLOCK_SIZE;
         // We begin by setting the entire bitmap to be inactive.
+        // Of course, they will still have access
         deinit_memory_region(0x0, 0xFFFFFFFF);
+        //memset(memory_map, 0xFF, max_blocks / BITMAP_DIVISON);
 }
