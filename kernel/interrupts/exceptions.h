20fe3c347cb6c2858fd4aaf6504500c77c4c8df5
abhatia06 <angadbhatia122@gmail.com>
2025-09-28 23:05:56 -0500

    Update exceptions.h

--- kernel/interrupts/exceptions.h
+++ kernel/interrupts/exceptions.h
@@ -1,6 +1,8 @@
 #pragma once
 
-extern char input_buffer[256];
+#define INPUT_BUFFER_SIZE 256
+
+extern char input_buffer[INPUT_BUFFER_SIZE];
 extern unsigned int input_pos;
 extern bool input_ready;
 extern volatile uint64_t ticks;
@@ -8,3 +10,5 @@ extern volatile uint64_t ticks;
 void div_by_0_handler(int_frame_32_t *frame);
 void keyboard_handler(int_frame_32_t *frame);
 void PIT_handler(int_frame_32_t *frame);
+void page_fault_handler(int_frame_32_t* frame, uint32_t error_code);
+void gpf_handler(int_frame_32_t* frame, uint32_t error_code);
